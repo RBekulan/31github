fc2111bb4017b7b6a8c2219dc001f11dd2df824a
RBekulan <bekulanrysmatov4@gmail.com>
2023-11-26 19:49:44 +0600

    users

---
+++ users/models.py
@@ -0,0 +1,9 @@
+# Create your models here.
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class UserConfirmation(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    code = models.IntegerField()
