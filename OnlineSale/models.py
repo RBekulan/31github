14f92a069b4411564ada31b9b76b56256d60273d
RBekulan <bekulanrysmatov4@gmail.com>
2023-11-28 23:05:13 +0600

    new

--- OnlineSale/models.py
+++ OnlineSale/models.py
@@ -21,6 +21,7 @@ class ReviewCom(models.Model):
     review = models.TextField()
     date = models.DateField(auto_now_add=True)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    date_create = models.DateField(auto_now_add=True, null=True, blank=True)
 
     def __str__(self):
         return self.review
