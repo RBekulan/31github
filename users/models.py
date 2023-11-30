5862489898728c3dbf81810fe8dc8d099efb4344
RBekulan <bekulanrysmatov4@gmail.com>
2023-11-30 23:09:43 +0600

    new

--- users/models.py
+++ users/models.py
@@ -7,3 +7,4 @@ from django.contrib.auth.models import User
 class UserConfirmation(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     code = models.IntegerField()
+
