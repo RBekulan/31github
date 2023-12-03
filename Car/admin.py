cabe49ea571837b6688d98e58fbcf93115cdafae
RBekulan <bekulanrysmatov4@gmail.com>
2023-12-03 22:50:37 +0600

    new

--- Car/admin.py
+++ Car/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import *
+from .models import Category, CarOnline, ReviewCom
 
 admin.site.register(Category)
 admin.site.register(CarOnline)
--- Car/views.py
+++ Car/views.py
@@ -35,3 +35,5 @@ class CarDetail(mixins.RetrieveModelMixin,
 
 
 
+
+
