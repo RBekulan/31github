6f0ea4c0e2d08e92a4835e9dc13a8e5a2cd08ff6
RBekulan <bekulanrysmatov4@gmail.com>
2023-12-09 20:40:27 +0600

    new

--- OnlineSale/views.py
+++ OnlineSale/views.py
@@ -1,45 +1,59 @@
-from rest_framework import mixins
-from .serializers import *
-from rest_framework import generics
-from .models import *
-from rest_framework import filters
-from rest_framework.response import Response
 
+from rest_framework import generics, mixins
+from .models import Product
+from .serializers import ProductsSerializer
+
+
+from rest_framework import generics, mixins
+from .models import Product
+from .serializers import ProductsSerializer
 
 class ProductList(mixins.ListModelMixin,
                   mixins.CreateModelMixin,
                   generics.GenericAPIView):
+    """
+    API endpoint that allows viewing a list of products or creating a new product.
+    """
     queryset = Product.objects.all()
     serializer_class = ProductsSerializer
 
     def get(self, request, *args, **kwargs):
+        """
+        Get a list of products.
+        """
         return self.list(request, *args, **kwargs)
 
     def post(self, request, *args, **kwargs):
+        """
+        Create a new product.
+        """
         return self.create(request, *args, **kwargs)
-
-
 
 
 class ProductDetail(mixins.RetrieveModelMixin,
                     mixins.UpdateModelMixin,
                     mixins.DestroyModelMixin,
                     generics.GenericAPIView):
+    """
+    API endpoint that allows viewing, updating, or deleting a specific product.
+    """
     queryset = Product.objects.all()
     serializer_class = ProductsSerializer
 
     def get(self, request, *args, **kwargs):
+        """
+        Retrieve a specific product instance.
+        """
         return self.retrieve(request, *args, **kwargs)
 
     def put(self, request, *args, **kwargs):
+        """
+        Update a specific product instance.
+        """
         return self.update(request, *args, **kwargs)
 
     def delete(self, request, *args, **kwargs):
+        """
+        Delete a specific product instance.
+        """
         return self.destroy(request, *args, **kwargs)
-
-# class SearchList(generics.ListCreateAPIView):
-#     def search(self, request):
-#         queryset = Product.objects.all()
-#         serializer_class = ProductsSerializer
-#         filter_backends = [filters.SearchFilter]
-#         search_fields = ['name', 'description']
