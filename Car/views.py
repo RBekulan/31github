556deac0df7ebd0f45da2c86c3d81b8015acbb8c
RBekulan <bekulanrysmatov4@gmail.com>
2023-12-10 16:40:50 +0600

    new

--- Car/views.py
+++ Car/views.py
@@ -11,6 +11,8 @@ class CarList(mixins.ListModelMixin,
     queryset = CarOnline.objects.all()
     serializer_class = CarOnlineSerializer
 
+
+
     def get(self, request, *args, **kwargs):
         """
         Get a list of cars online.
